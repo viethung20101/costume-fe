21cfcd6bbc4c46076f00211442f712f798d0b25e
Wezeaemon <shatomon12345@gmail.com>
2025-09-08 15:13:57 +0700

    Update 9/8/2025
    Update Script ModelSkinRendererAsianFemale

--- Assets/Script/ModelSkinRendererAsianFemale.cs
+++ Assets/Script/ModelSkinRendererAsianFemale.cs
@@ -2,15 +2,52 @@ using UnityEngine;
 
 public class ModelSkinRendererAsianFemale : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] SkinnedMeshRenderer skinnedMeshRenderer;
+    [SerializeField] Mesh mesh;
+    [SerializeField] string[] Empty;
+    [SerializeField] private float weigth;
+    [SerializeField] private float Growbigger;
+    [SerializeField] private float[] Limit;
+    private void Awake()
     {
         
     }
+    void Start()
+    {
+       skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();    
+       mesh = skinnedMeshRenderer.sharedMesh;
+    }
 
-    // Update is called once per frame
+  
     void Update()
     {
-        
+        if(Growbigger < Limit[0])
+        {
+           Growbigger += 0.3f;
+        }
+        for(int i = 0; i < Empty.Length; i++)
+        {
+            
+             GetBlendShape(Empty[i], weigth);
+            
+        } 
+       
+    }
+    public void GetBlendShape(string ShapeName, float weigth)
+    {
+        int index = mesh.GetBlendShapeIndex(ShapeName);
+        if(index >= 0)
+        {
+                skinnedMeshRenderer.SetBlendShapeWeight(index, weigth);
+        }
+        else
+        {
+            Debug.Log("Không tìm thấy BlendShape mà bạn tìm");
+        }
+    }
+    public void Look()
+    {
+         GetBlendShape(Empty[1], 100);
+         GetBlendShape(Empty[3], 100);
     }
 }
