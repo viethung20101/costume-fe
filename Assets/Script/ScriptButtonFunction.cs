7d9583c48ed6250046fb6bb27a637e79726af8e4
Wezeaemon <shatomon12345@gmail.com>
2025-09-21 14:09:22 +0700

    Update 21/9/2025
    Add Script CanvaPlayer And Update ScriptButtonFunction

--- Assets/Script/ScriptButtonFunction.cs
+++ Assets/Script/ScriptButtonFunction.cs
@@ -1,24 +1,41 @@
 using UnityEngine;
 using UnityEngine.UI; 
+using DG.Tweening;
 public class ScriptButtonFunction : MonoBehaviour
 {
     [SerializeField] Button button;
     [SerializeField] GameObject[] Model_Character;
+    [SerializeField] private Vector3 InitalScale;
+    private void Awake()
+    {
+        InitalScale = transform.localScale;
+    }
     void Start()
     {
      
     }
    public void On_Button()
    {
-    button.interactable = true; 
+    InCreateScale(true);
+      button.interactable = false; 
+ 
     for(int i = 0; i < Model_Character.Length; i++)
     {
         Model_Character[i].SetActive(false);
+       
     }
     Model_Character[0].SetActive(true);
    }
    public void Off_Button()
    {
-    button.interactable = false; 
+    InCreateScale(false);
+       button.interactable = true; 
+   }
+   private void InCreateScale(bool isScale)
+   {
+    Vector3 finalScale = InitalScale;
+    if(isScale)
+    finalScale = InitalScale * 2f;
+     transform.DOScale(finalScale, 0.15f);
    }
 }
