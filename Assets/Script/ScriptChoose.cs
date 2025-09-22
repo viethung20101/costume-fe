0cf916b7fbd9355c4599ff35e051fc49cd344961
Wezeaemon <shatomon12345@gmail.com>
2025-09-22 15:15:15 +0700

    Update 22/9/2025
    add ScriptSourceScene

--- Assets/Script/ScriptChoose.cs
+++ Assets/Script/ScriptChoose.cs
@@ -1,9 +1,20 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using System.Collections;
+using DG.Tweening;
 public class ScriptChoose : MonoBehaviour
 {
    [SerializeField]  private string nameScene;
-
+   [SerializeField] private Vector3 InitalScale;
+   [SerializeField] private float Range;
+   [SerializeField] Transform FeetPos;
+   [SerializeField] Button button;
+      private void Awake()
+    {
+        InitalScale = transform.localScale;
+    }
     void Start()
     {
         
@@ -11,6 +22,25 @@ public class ScriptChoose : MonoBehaviour
 
    public void Choose()
    {
-    SceneManager.LoadScene(nameScene);
+      InCreateScale(true);
+      StartCoroutine(StopTemporary());
+      button.interactable = false;
+   }
+    public void Off_Button()
+   {
+    InCreateScale(false);
+    button.interactable = true;
+   }
+   IEnumerator StopTemporary()
+   {
+      yield return new WaitForSeconds(Range);
+       SceneManager.LoadScene(nameScene);
+   }
+    private void InCreateScale(bool isScale)
+   {
+    Vector3 finalScale = InitalScale;
+    if(isScale)
+    finalScale = InitalScale * 2f;
+     FeetPos.DOScale(finalScale, 0.15f);
    }
 }
---
+++ Assets/Script/ScriptSourceScene.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+public class ScriptSourceScene : MonoBehaviour
+{
+    static public int _Source;
+    public int D_Source;
+    private void Awake()
+    {
+       _Source = 1;
+        if(PlayerPrefs.HasKey("HighSource"))
+        {
+           Source = PlayerPrefs.GetInt("HighSource");
+        }
+        else
+        {
+           PlayerPrefs.GetInt("HighSource",Source);
+        }
+    }
+    static public int Source
+  {
+     
+        get{return _Source;}
+        private set{
+         _Source = value;
+        PlayerPrefs.SetInt("HighSource", value);
+        }
+  }
+    
+  
+}
